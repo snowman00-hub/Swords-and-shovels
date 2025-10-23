14064f69796147be3300b390b70e09659f4ec0aa
박의현 <uihyun420@gmail.com>
2025-10-23 16:43:26 +0900

    몬스터 플레이어 감지 후 이동

--- swords-and-shovels/Assets/Scripts/MonsterBehavior.cs
+++ swords-and-shovels/Assets/Scripts/MonsterBehavior.cs
@@ -1,6 +1,8 @@
 using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.CompilerServices;
 using System;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MonsterBehavior : MonoBehaviour
@@ -13,6 +15,10 @@ public class MonsterBehavior : MonoBehaviour
     private float attackPower = 10;
     private float attackInterval = 1f;
     private bool isInTrigger = false;
+    private bool isMovingToPlayer = false;
+    private float detectedRange = 5f;
+    private float moveSpeed = 5f;
+    private float stopDistance = 2f;
 
 
     private void OnEnable()
@@ -25,12 +31,14 @@ public class MonsterBehavior : MonoBehaviour
         {
             monsterHealth = GetComponent<MonsterHealth>();
         }
+        TrackPlayer().Forget();
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(playerTag))
         {
             isInTrigger = true;
+            isMovingToPlayer = false;
             AsyncAttack().Forget();
         }
     }
@@ -45,6 +53,7 @@ public class MonsterBehavior : MonoBehaviour
     public void OnPlayerDead()
     {
         isInTrigger = false;
+        isMovingToPlayer = false;
         player = null;
     }
 
@@ -73,5 +82,47 @@ public class MonsterBehavior : MonoBehaviour
             await UniTask.Delay(TimeSpan.FromSeconds(attackInterval));
         }
     } 
+
+    private async UniTaskVoid TrackPlayer()
+    {
+        while(monsterHealth != null && !monsterHealth.isDead)
+        {
+            if(player != null && !player.isDead)
+            {
+                float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+                if(distanceToPlayer <= detectedRange && distanceToPlayer > stopDistance && !isInTrigger)
+                {
+                    isMovingToPlayer = true;
+                    MoveTowardsPlayer();
+                }
+                else if (distanceToPlayer > detectedRange)
+                {
+                    isMovingToPlayer = false;
+                }                
+            }
+            else
+            {
+                isMovingToPlayer= false;
+            }
+            await UniTask.Delay(100);
+        }
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        if (player != null && isMovingToPlayer && !isInTrigger)
+        {
+            Vector3 direction = (player.transform.position - transform.position).normalized;
+            direction.y = 0; 
+
+            transform.position += direction * moveSpeed * Time.deltaTime;
+
+            if (direction != Vector3.zero)
+            {
+                transform.rotation = Quaternion.LookRotation(direction);
+            }
+        }
+    }
+
 
 }
