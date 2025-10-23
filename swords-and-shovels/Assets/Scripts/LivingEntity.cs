25f186587648174afe4af74b6b3da604395cddb1
박의현 <uihyun420@gmail.com>
2025-10-23 11:53:45 +0900

    플레이어 공격

---
+++ swords-and-shovels/Assets/Scripts/LivingEntity.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class LivingEntity : MonoBehaviour, IDamageable
+{
+    public bool isDead { get; set; }
+    public float currentHp { get; set; }
+    public float currentMp { get; set; }
+
+    protected virtual void OnEnable()
+    {
+        isDead = false;
+    }
+
+    public virtual void Ondamage(float damage, Vector3 hitPosition)
+    {
+        currentHp -= damage;
+        if(currentHp <= 0)
+        {
+            currentHp = 0;
+            gameObject.SetActive(false);
+            isDead = true;
+        }
+    }
+}
---
+++ swords-and-shovels/Assets/Scripts/MonsterHealth.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MonsterHealth : LivingEntity
+{
+    private float monsterMaxHealth = 30f;
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        currentHp = monsterMaxHealth;
+        gameObject.SetActive(true);
+    }
+
+    public override void Ondamage(float damage, Vector3 hitPosition)
+    {
+        if (isDead) return;
+
+        base.Ondamage(damage, hitPosition);
+    }
+}
